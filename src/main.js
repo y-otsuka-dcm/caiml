124c2b4781031a4b43dc16b7c9c75d0da378bc26
Yoshiki Otsuka <lime@YoshikinoMacBook-ea.local>
2019-07-01 02:26:23 +0900

    big change

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-// Vue.config.productionTip = false
+import BootstrapVue from 'bootstrap-vue'
+import VeeValidate from 'vee-validate'
+
+Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(VeeValidate)
 
 /* eslint-disable no-new */
 new Vue({
